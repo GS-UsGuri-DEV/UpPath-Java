ed94dc0ef33ae01920d89db8e238cb437e862e1a
alex-isidro <alexander.d.isidro@gmail.com>
2025-11-14 02:22:14 -0300

    feat: adicionando campos de imagem e descrição na classe Courses e atualizando métodos relacionados

--- src/main/java/br/com/fiap/bo/CoursesBO.java
+++ src/main/java/br/com/fiap/bo/CoursesBO.java
@@ -2,6 +2,7 @@ package br.com.fiap.bo;
 
 import br.com.fiap.TO.Courses;
 import br.com.fiap.dao.CoursesDAO;
+import br.com.fiap.service.IdGenerator;
 
 import java.util.ArrayList;
 
@@ -20,8 +21,11 @@ public class CoursesBO {
 
     public Courses save(Courses courses) {
         coursesDAO = new CoursesDAO();
+        long id = IdGenerator.generate("CURSOS", "id_curso");
+        courses.setIdCourse(id);
         return coursesDAO.save(courses);
     }
+
     public boolean delete(Long id) {
         coursesDAO = new CoursesDAO();
         return coursesDAO.delete(id);
